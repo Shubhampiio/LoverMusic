4b16ff1d5b9a655963b54094ce59fcff9417db68
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 22:27:45 -0800

    Update call.py

--- LoverMusic/core/call.py
+++ LoverMusic/core/call.py
@@ -18,9 +18,9 @@ from pytgcalls.types.stream import StreamAudioEnded
 
 import config
 from strings import get_string
-from AnonX import LOGGER, YouTube, app
-from AnonX.misc import db
-from AnonX.utils.database import (add_active_chat,
+from LoverMusic import LOGGER, YouTube, app
+from LoverMusic.misc import db
+from LoverMusic.utils.database import (add_active_chat,
                                        add_active_video_chat,
                                        get_assistant,
                                        get_audio_bitrate, get_lang,
@@ -29,11 +29,11 @@ from AnonX.utils.database import (add_active_chat,
                                        music_on, set_loop,
                                        remove_active_chat,
                                        remove_active_video_chat)
-from AnonX.utils.exceptions import AssistantErr
-from AnonX.utils.inline.play import (stream_markup,
+from LoverMusic.utils.exceptions import AssistantErr
+from LoverMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
-from AnonX.utils.stream.autoclear import auto_clean
-from AnonX.utils.thumbnails import gen_thumb
+from LoverMusic.utils.stream.autoclear import auto_clean
+from LoverMusic.utils.thumbnails import gen_thumb
 
 autoend = {}
 counter = {}
