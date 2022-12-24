38a8bbdfa5d6c50135fbef3770933ebd1fa5654a
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-24 00:27:25 -0800

    Update queue.py

--- LoverMusic/utils/stream/queue.py
+++ LoverMusic/utils/stream/queue.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from AnonX.misc import db
+from LoverMusic.misc import db
 
 
 async def put_queue(
