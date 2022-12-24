2dbccff3a5bcfc1f9dfb38300cdfed2924af0344
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-24 00:21:50 -0800

    Update help.py

--- LoverMusic/utils/inline/help.py
+++ LoverMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonX import app
+from LoverMusic import app
 from config import SUPPORT_GROUP
 
 
