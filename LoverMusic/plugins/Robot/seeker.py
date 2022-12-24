d27c360dda0fd9a207e6a88d3075599d80288d34
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:43:18 -0800

    Update seeker.py

--- LoverMusic/plugins/Robot/seeker.py
+++ LoverMusic/plugins/Robot/seeker.py
@@ -3,14 +3,14 @@ import asyncio
 from pyrogram.types import InlineKeyboardMarkup
 
 from strings import get_string
-from AnonX.misc import db
-from AnonX.utils.database import (get_active_chats, get_lang,
+from LoverMusic.misc import db
+from LoverMusic.utils.database import (get_active_chats, get_lang,
                                        is_music_playing)
-from AnonX.utils.formatters import seconds_to_min
-from AnonX.utils.inline import (stream_markup_timer,
+from LoverMusic.utils.formatters import seconds_to_min
+from LoveMusic.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
-from AnonX.plugins.Robot.callback import wrong
+from LoverMusic.plugins.Robot.callback import wrong
 
 checker = {}
 
