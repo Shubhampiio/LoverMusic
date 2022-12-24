abde7b1762975529deb50835e2ae2609cf28d7f6
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:02:20 -0800

    Update live.py

--- LoverMusic/plugins/Robot/live.py
+++ LoverMusic/plugins/Robot/live.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from AnonX import YouTube, app
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.stream.stream import stream
+from LoverMusic import YouTube, app
+from LoverMusic.utils.channelplay import get_channeplayCB
+from LoverMusic.utils.decorators.language import languageCB
+from LoverMusic.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
