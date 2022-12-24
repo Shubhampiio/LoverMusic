e0b3535e5e20d16a2cb9960559bac99743b69f6b
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:55:17 -0800

    Update speedtest.py

--- LoverMusic/plugins/Robot/speedtest.py
+++ LoverMusic/plugins/Robot/speedtest.py
@@ -2,8 +2,8 @@ import asyncio
 import speedtest
 from pyrogram import filters
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
