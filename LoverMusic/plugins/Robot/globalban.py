49d2a451c280f1ec446f995a00211681358b025a
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 22:56:17 -0800

    Update globalban.py

--- LoverMusic/plugins/Robot/globalban.py
+++ LoverMusic/plugins/Robot/globalban.py
@@ -7,16 +7,16 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils import get_readable_time
-from AnonX.utils.database import (add_banned_user,
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils import get_readable_time
+from LoverMusic.utils.database import (add_banned_user,
                                        get_banned_count,
                                        get_banned_users,
                                        get_served_chats,
                                        is_banned_user,
                                        remove_banned_user)
-from AnonX.utils.decorators.language import language
+from LoverMusic.utils.decorators.language import language
 
 # Command
 GBAN_COMMAND = get_command("GBAN_COMMAND")
