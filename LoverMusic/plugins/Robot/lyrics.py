e3682b00aa002aa52b85afa99efe31c593508bb5
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:06:31 -0800

    Update lyrics.py

--- LoverMusic/plugins/Robot/lyrics.py
+++ LoverMusic/plugins/Robot/lyrics.py
@@ -9,8 +9,8 @@ from pyrogram.types import (InlineKeyboardButton,
 
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AnonX import app
-from AnonX.utils.decorators.language import language
+from LoverMusic import app
+from LoverMusic.utils.decorators.language import language
 
 ###Commands
 LYRICS_COMMAND = get_command("LYRICS_COMMAND")
