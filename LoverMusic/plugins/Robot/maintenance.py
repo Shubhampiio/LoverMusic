8c24b495b75b3e594e9b35e7da622577b079bbf9
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:07:40 -0800

    Update maintenance.py

--- LoverMusic/plugins/Robot/maintenance.py
+++ LoverMusic/plugins/Robot/maintenance.py
@@ -2,12 +2,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command, get_string
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (get_lang, is_maintenance,
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils.database import (get_lang, is_maintenance,
                                        maintenance_off,
                                        maintenance_on)
-from AnonX.utils.decorators.language import language
+from LoverMusic.utils.decorators.language import language
 
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
