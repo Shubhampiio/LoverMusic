c9bdbcd08760e72d58bfbb11faa74a99ef74c654
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:41:47 -0800

    Update seek.py

--- LoverMusic/plugins/Robot/seek.py
+++ LoverMusic/plugins/Robot/seek.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import YouTube, app
-from AnonX.core.call import Anon
-from AnonX.misc import db
-from AnonX.utils import AdminRightsCheck, seconds_to_min
+from LoverMusic import YouTube, app
+from LoverMusic.core.call import Anon
+from LoverMusic.misc import db
+from LoverMusic.utils import AdminRightsCheck, seconds_to_min
 
 # Commands
 SEEK_COMMAND = get_command("SEEK_COMMAND")
