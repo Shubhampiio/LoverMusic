5b92695259e9c50bdfea86a01766e8bb737e7fee
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-24 00:35:09 -0800

    Update logger.py

--- LoverMusic/utils/logger.py
+++ LoverMusic/utils/logger.py
@@ -1,6 +1,6 @@
 from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
-from AnonX import app
-from AnonX.utils.database import is_on_off
+from LoverMusic import app
+from LoverMusic.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
