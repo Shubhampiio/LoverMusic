63dd7e3138ae94129cb721dbd55f0deebbb97b00
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 22:57:47 -0800

    Update help.py

--- LoverMusic/plugins/Robot/help.py
+++ LoverMusic/plugins/Robot/help.py
@@ -6,13 +6,13 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 import config
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils import help_pannel
-from AnonX.utils.database import get_lang, is_commanddelete_on
-from AnonX.utils.decorators.language import (LanguageStart,
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils import help_pannel
+from LoverMusic.utils.database import get_lang, is_commanddelete_on
+from LoverMusic.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from AnonX.utils.inline.help import (help_back_markup,
+from LoverMusic.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
