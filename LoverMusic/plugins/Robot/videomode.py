911ea59bf02fe19e1f7ba77fd33eeb54cd98c561
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-24 00:11:29 -0800

    Update videomode.py

--- LoverMusic/plugins/Robot/videomode.py
+++ LoverMusic/plugins/Robot/videomode.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import add_off, add_on
-from AnonX.utils.decorators.language import language
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils.database import add_off, add_on
+from LoverMusic.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
