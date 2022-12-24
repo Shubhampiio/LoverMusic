28c1ea6f6429885dcbae199ca31a1eccf67fcb96
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 22:35:08 -0800

    Update active.py

--- LoverMusic/plugins/Robot/active.py
+++ LoverMusic/plugins/Robot/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database.memorydatabase import (
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils.database.memorydatabase import (
     get_active_chats, get_active_video_chats)
 
 # Commands
