ce7f27894b547e10bc1b10c081ec5c4789eac03a
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-24 13:14:48 +0530

    Update help.py

--- LoverMusic/plugins/Robot/help.py
+++ LoverMusic/plugins/Robot/help.py
@@ -89,7 +89,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "ʜᴀᴀɴ ᴀᴀᴊᴀ ʙsᴅᴋ ʟᴜɴᴅ ʟᴇʟᴇ ᴍᴇʀᴀ.\n\nᴊʜᴀᴀᴛ ʙᴀʀᴀᴀʙᴀʀ ᴅɪᴍᴀᴀɢ ʜᴀɪ ɴᴀʜɪ ᴀᴜʀ ᴏᴡɴᴇʀ ʙᴀɴᴇɴɢᴇ.ᴛᴇʀᴀ ᴋʜᴜᴅᴋᴀ ʙɴᴀᴠᴀɴᴀ ʜᴇ ᴛᴏ ᴄᴏɴᴇᴛᴄᴛ ᴋᴀʀʟᴇ ᴘʀᴏ ᴅᴇᴠᴇʟᴏᴘᴇʀ ꜱᴇ @ll_ll_LegendHacker_IN_ll_ll", show_alert=True
+                "ʜᴀᴀɴ ᴀᴀᴊᴀ ʙsᴅᴋ ʟᴜɴᴅ ʟᴇʟᴇ ᴍᴇʀᴀ.\n\nᴊʜᴀᴀᴛ ʙᴀʀᴀᴀʙᴀʀ ᴅɪᴍᴀᴀɢ ʜᴀɪ ɴᴀʜɪ ᴀᴜʀ ᴏᴡɴᴇʀ ʙᴀɴᴇɴɢᴇ.ᴛᴇʀᴀ ᴋʜᴜᴅᴋᴀ ʙɴᴀᴠᴀɴᴀ ʜᴇ ᴛᴏ ᴄᴏɴᴇᴛᴄᴛ ᴋᴀʀʟᴇ ᴘʀᴏ ᴅᴇᴠᴇʟᴏᴘᴇʀ ꜱᴇ @shubhamsah1", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
