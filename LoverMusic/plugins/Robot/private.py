c6ac462ac029f7cb04273ed4d104c6dde86da48d
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-23 23:32:34 -0800

    Update private.py

--- LoverMusic/plugins/Robot/private.py
+++ LoverMusic/plugins/Robot/private.py
@@ -3,13 +3,13 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (add_private_chat,
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils.database import (add_private_chat,
                                        get_private_served_chats,
                                        is_served_private_chat,
                                        remove_private_chat)
-from AnonX.utils.decorators.language import language
+from LoverMusic.utils.decorators.language import language
 
 AUTHORIZE_COMMAND = get_command("AUTHORIZE_COMMAND")
 UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
