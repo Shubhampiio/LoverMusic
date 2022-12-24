c0578c26dc5ea8c671286a5698b4e0a9de1d5069
Shubhampiio <121330390+Shubhampiio@users.noreply.github.com>
2022-12-24 00:10:16 -0800

    Update videolimit.py

--- LoverMusic/plugins/Robot/videolimit.py
+++ LoverMusic/plugins/Robot/videolimit.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import set_video_limit
-from AnonX.utils.decorators.language import language
+from LoverMusic import app
+from LoverMusic.misc import SUDOERS
+from LoverMusic.utils.database import set_video_limit
+from LoverMusic.utils.decorators.language import language
 
 VIDEOLIMIT_COMMAND = get_command("VIDEOLIMIT_COMMAND")
 
